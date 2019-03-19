4703b93a9858a34ef900f24d64f41712d1200957
github username <github email address>
2019-03-19 09:09:15 -0400

    done

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,10 @@
+require 'pry'
 def count_elements(array)
-  # code goes here
+  hash = {}
+  array.each do |item|
+    count = 1
+    hash.has_key?(item)? hash[item] +=1 : hash.store(item, count)
+  end
+  hash
 end
  
